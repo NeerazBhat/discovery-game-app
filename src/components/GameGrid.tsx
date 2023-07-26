2595f1371863f175f04c005460b6214eaa861588
NeerazBhat <neeraz.bhat@gmail.com>
2023-07-26 13:03:37 +0545

    feat: created on hook and used AbortController

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Spinner, Text } from "@chakra-ui/react";
-
-interface IGame {
-  id: number;
-  name: string;
-}
-
-interface IFetchGameResponse {
-  count: number;
-  results: IGame[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<IGame[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    apiClient
-      .get<IFetchGameResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-  }, []);
+  const { error, games, isLoading } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface IGame {
+  id: number;
+  name: string;
+}
+
+interface IFetchGameResponse {
+  count: number;
+  results: IGame[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<IGame[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<IFetchGameResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
+
+export default useGames;
