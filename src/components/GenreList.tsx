adf34290ee52ce6fc5aa9b242abcebf581c4dfcc
NeerazBhat <neeraz.bhat@gmail.com>
2023-08-03 11:47:21 +0545

    feat: added skeleton loader in genrelist

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,23 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getImageCroppedUrl from "../services/img-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading } = useGenres();
+  const skeletonGenrelists = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <List padding={4}>
+      {isLoading &&
+        skeletonGenrelists.map((list) => (
+          <Skeleton height="40px" marginY={2} key={list} />
+        ))}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY={2}>
           <HStack>
@@ -14,7 +26,9 @@ const GenreList = () => {
               boxSize="32px"
               borderRadius={8}
             />
-            <Text color='white' fontSize='xl'>{genre.name}</Text>
+            <Text color="white" fontSize="xl">
+              {genre.name}
+            </Text>
           </HStack>
         </ListItem>
       ))}
