98be14f35ad0fa973628fcb56aab1332263a1d8e
NeerazBhat <neeraz.bhat@gmail.com>
2023-08-20 15:08:08 +0545

    refactor: use static data for genres and platforms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,10 @@
-import useData from "./useData";
-
+import genres from "../data/genres";
 export interface IGenre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => useData<IGenre>("/genres");
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,5 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
 
-interface IPlatform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
-const usePlatforms = () => useData<IPlatform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 
 export default usePlatforms;
