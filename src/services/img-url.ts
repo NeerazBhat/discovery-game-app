2c3947f9b6aed1b11fc34645300018404b44a1fa
NeerazBhat <neeraz.bhat@gmail.com>
2023-08-20 09:48:00 +0545

    feat: added placeholder image on no image

--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -1,5 +1,7 @@
+import placeholderImg from "../assets/placeholder-image.jpeg";
+
 const getImageCroppedUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeholderImg;
   const index = url.indexOf("media/") + "media/".length;
   const newUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
   return newUrl;
